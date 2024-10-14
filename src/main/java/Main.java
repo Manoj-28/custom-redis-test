062b38c4fd2f08186744d8d2ecd51863d0f398d4
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-14 20:32:27 +0530

    support multiple ping response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class Main {
@@ -14,7 +17,16 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStream out = clientSocket.getOutputStream();
+
+            String inputLine;
+
+            while ((inputLine = reader.readLine())!= null) {
+                if(inputLine.equals("PING")){
+                    out.write("+PONG\r\n".getBytes());
+                }
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
