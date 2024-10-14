a3f712ac23283d4c65b07b726f078e60fc6bbd74
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-14 21:03:00 +0530

    support multiple ping response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
             // Since the tester restarts your program quite often, setting
             // SO_REUSEADDR ensures that we don't run into 'Address already in use'
             // errors
+            
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
