cd4ad846b5848bde4dd3b7c443398055fbcd662e
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-20 10:51:34 +0530

    added set and get

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.HashMap;
 
 // Thread to handle client communication
 class ClientHandler extends Thread {
     private Socket clientSocket;
+    public static Map<String, String> KeyValueStore = new HashMap<>();
 
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
@@ -22,15 +25,33 @@ class ClientHandler extends Thread {
 
             while (true) {
                 String inputLine = reader.readLine();
-                System.out.println("InputLine: "+inputLine);
-                if (inputLine.equals("PING")) {
-                    out.write("+PONG\r\n".getBytes());
+//                System.out.println("Inputline: "+inputLine);
+                if (inputLine == null) break;
+
+                if(inputLine.startsWith("*")){
+                    String[] commandParts = parseRespCommand(reader, inputLine);
+                    if(commandParts != null && commandParts.length > 0){
+                        String command = commandParts[0].toUpperCase();
+
+                        switch (command){
+                            case "PING":
+                                out.write("+PONG\r\n".getBytes());
+                                break;
+                            case "ECHO":
+                                if(commandParts.length > 1){
+                                    String message = commandParts[1];
+                                    out.write(String.format("$%\d\r\n%s\r\n", message.length(), message).getBytes());
+                                }
+                            case "SET":
+                                handleSetCommand(commandParts,out);
+                            case "GET":
+                                handleGetCommand(commandParts, out);
+                            default:
+                                out.write("-ERR unknown command\r\n".getBytes());
+                        }
+                    }
                 }
-                else if(inputLine.equalsIgnoreCase("ECHO")){
-                    reader.readLine();
-                    String message = reader.readLine();
-                    out.write(String.format("$%d\r\n%s\r\n", message.length(), message).getBytes());
-                }
+
             }
         } catch (IOException e) {
             System.out.println("IOException in client handler: " + e.getMessage());
@@ -42,6 +63,51 @@ class ClientHandler extends Thread {
             } catch (IOException e) {
                 System.out.println("IOException when closing client socket: " + e.getMessage());
             }
+        }
+    }
+
+    private String[] parseRespCommand(BufferedReader reader, String firstline) throws IOException{
+        int numElements = Integer.parseInt(firstline.substring(1));
+        String[] commandParts = new String[numElements];
+
+        for(int i=0;i<numElements;i++){
+            String lenghtLine = reader.readLine();
+            if(lenghtLine.startsWith("$")){
+                String bulkString = reader.readLine();
+                commandParts[i] = bulkString;
+            }
+        }
+        System.out.println("Parsed RESP Command: " + String.join(", ", commandParts));
+        return commandParts;
+    }
+
+    private void handleSetCommand(String[] commandParts, OutputStream out) throws IOException {
+        if (commandParts.length < 3) {
+            out.write("-ERR wrong number of arguments for 'SET' command\r\n".getBytes());
+            return;
+        }
+        String key = commandParts[1];
+        String value = commandParts[2];
+
+        KeyValueStore.put(key, value);
+
+        out.write("+OK\r\n".getBytes());
+    }
+    private void handleGetCommand(String[] commandParts, OutputStream out) throws IOException{
+
+        if(commandParts.length < 2){
+            out.write("-ERR wrong number of arguments for 'GET' command\r\n".getBytes());
+            return;
+        }
+
+        String key = commandParts[1];
+        String value = KeyValueStore.get(key);
+
+        if(value != null){
+            out.write(String.format("$%d\r\n%s\r\n", value.length(), value).getBytes());
+        }
+        else{
+            out.write("$-1\r\n".getBytes());
         }
     }
 }
