dc3c1ac8327c9b93778712dc7a1dab273cbda3ad
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-20 10:33:05 +0530

    added set and get

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Map;
-import java.util.HashMap;
 
 // Thread to handle client communication
 class ClientHandler extends Thread {
     private Socket clientSocket;
-    public static Map<String, String> KeyValueStore = new HashMap<>();
 
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
@@ -25,24 +22,15 @@ class ClientHandler extends Thread {
 
             while (true) {
                 String inputLine = reader.readLine();
-                System.out.println("Inputline: "+inputLine);
-                if (inputLine == null) break;
-
-                String[] commandParts = inputLine.split("\r\n");
-                System.out.println("cd0"+commandParts[0]);
-                String command = commandParts[0].toUpperCase();
-                if (command.equals("PING")) {
+                System.out.println("InputLine: "+inputLine);
+                if (inputLine.equals("PING")) {
                     out.write("+PONG\r\n".getBytes());
-                } else if (command.equals("ECHO")) {
+                }
+                else if(inputLine.equalsIgnoreCase("ECHO")){
                     reader.readLine();
                     String message = reader.readLine();
                     out.write(String.format("$%d\r\n%s\r\n", message.length(), message).getBytes());
-                } else if (command.equals("SET")) {
-                    handleSetCommand(commandParts, out);
-                } else if (command.equals(("GET"))) {
-                    handleGetCommand(commandParts, out);
                 }
-
             }
         } catch (IOException e) {
             System.out.println("IOException in client handler: " + e.getMessage());
@@ -56,36 +44,6 @@ class ClientHandler extends Thread {
             }
         }
     }
-
-    private void handleSetCommand(String[] commandParts, OutputStream out) throws IOException {
-        if (commandParts.length < 3) {
-            out.write("-ERR wrong number of arguments for 'SET' command\r\n".getBytes());
-            return;
-        }
-        String key = commandParts[1];
-        String value = commandParts[2];
-
-        KeyValueStore.put(key, value);
-
-        out.write("+OK\r\n".getBytes());
-    }
-     private void handleGetCommand(String[] commandParts, OutputStream out) throws IOException{
-
-        if(commandParts.length < 2){
-            out.write("-ERR wrong number of arguments for 'GET' command\r\n".getBytes());
-            return;
-        }
-
-        String key = commandParts[1];
-        String value = KeyValueStore.get(key);
-
-        if(value != null){
-            out.write(String.format("$%d\r\n%s\r\n", value.length(), value).getBytes());
-        }
-        else{
-            out.write("$-1\r\n".getBytes());
-        }
-     }
 }
 
 public class Main {
