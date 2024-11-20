81df86880fb9a6c43e3202ed0af2df070dfab44d
Manoj-28 <manojbhoye9928@gmail.com>
2024-11-20 12:16:03 +0530

    Command Processing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -291,7 +291,7 @@ class ClientHandler extends Thread {
 
 public class Main {
 
-    private static final CountDownLatch latch = new CountDownLatch(1);
+    private static final CountDownLatch latch = new CountDownLatch(5);
 
     public static void main(String[] args) {
         int port = 6379;  // Default port
