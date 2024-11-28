16942e1cb2054eb6919c64dbaf54dde97859e1ba
Manoj-28 <manojbhoye9928@gmail.com>
2024-11-28 09:37:49 +0530

    added wait with multiple cmds

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,7 +37,7 @@ class ClientHandler extends Thread {
     private static final long REPLICATION_OFFSET = 0;
     private static final Map<Long, Integer> replicaAcknowledgment = new HashMap<>();
     private static final Object waitLock = 0;
-    static long currentOffset = 0;
+    static long currentOffset = -1;
 
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
@@ -111,6 +111,7 @@ class ClientHandler extends Thread {
         String respCommand = String.format("*3\r\n$3\r\nSET\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", key.length(), key, value.length(), value);
         String ackCommand  = "*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n";
 
+        currentOffset++;
         synchronized (waitLock){
             replicaAcknowledgment.put(currentOffset,0);
         }
@@ -126,7 +127,6 @@ class ClientHandler extends Thread {
                 System.out.println("Failed to send commands to replica: "  +e.getMessage());
             }
         }
-//        currentOffset++;
     }
     private void handleGetCommand(String[] commandParts, OutputStream out) throws IOException{
 
